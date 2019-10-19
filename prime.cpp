2d23d21f0367babb78575a79523cfa3035184049
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-10-19 09:07:56 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -28,3 +28,31 @@ void main(){
 
     getch();
 }
+
+//  I have written more effective code for you to reduce the time analysis and making it effective
+
+/*
+
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+  int n, i, m=0, flag=0;  
+  cout << "Enter the Number to check Prime: ";  
+  cin >> n;  
+  m=n/2;  
+  for(i = 2; i <= m; i++)  
+  {  
+      if(n % i == 0)  
+      {  
+          cout<<"Number is not Prime."<<endl;  
+          flag=1;  
+          break;  
+      }  
+  }  
+  if (flag==0)  
+      cout << "Number is Prime."<<endl;  
+  return 0;  
+} 
+
+*/ 
