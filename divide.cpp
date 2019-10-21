010dfdfe9ebd589c663bbc78c9c4756a233f5aa8
Sanya Verma <sanyaverma0303@gmail.com>
2019-10-21 22:02:34 +0530

    Divisor CPP (#31)
    * first commit
    
    * Create divide.cpp

---
+++ divide.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main(){
+  int i,j,k;
+  cout<<"Enter 2 divide";
+  
+  cin>>i,j;
+  k=i/j;
+  
+  cout<<"Divide: "<<k;
+  
+  getch();
+  
+}
---
+++ matrix.cpp
@@ -0,0 +1,7 @@
+#include<iostream.h>
+#include<conio.h>
+
+int main() {
+  cout << "Knock, knock Neo." << endl;
+  return 0;
+ }
